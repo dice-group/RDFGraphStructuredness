f6ffe9a1202ecd09712be9e98700025152c9c0a1
diceci <60946684+diceci@users.noreply.github.com>
2020-05-05 14:04:32 +0200

    updated description

--- setup.py
+++ setup.py
@@ -8,11 +8,7 @@ setuptools.setup(
 	version="0.1.0",
 	author="Alexander Bigerl",
 	author_email="bigerl@mail.upb.de",
-	description='Calculate the structuredness or coherence of a dataset as defined in Duan et al. paper titled '
-				'"Apples and Oranges: A Comparison of RDF Benchmarks and Real RDF Datasets". The structuredness is '
-				'measured in interval [0,1] with values close to 0 corresponding to low structuredness, '
-				'and 1 corresponding to perfect structuredness. The paper concluded that synthetic datasets have high '
-				'structurenes as compared to real datasets.',
+	description='Calculate the structuredness or coherence of an RDF dataset.',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/dice-group/RDFGraphStructuredness",
